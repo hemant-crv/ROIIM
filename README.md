6b48010b738b3586aad90cb972b53ecdf170d51b
Hemant Choudhary <21359905+hemant-crv@users.noreply.github.com>
2021-03-19 17:31:38 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# PaySafe-Task
+1) install dependencies in both frontend and backend using npm install.
+2) run backend dev server using 'node index'
+3) run frontend dev server using 'npm start'
+use this link for testing cards
+https://developer.paysafe.com/en/rest-apis/cards/test-and-go-live/test-cards/
---
+++ backend/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db: 'mongodb+srv://test_growindigo:G945QmevW51M9k9g@cluster0.o8fgt.mongodb.net/USER_DETAILS?retryWrites=true&w=majority',
+    ps_key: 'Basic cHJpdmF0ZS03NzUxOkItcWEyLTAtNWYwMzFjZGQtMC0zMDJkMDIxNDQ5NmJlODQ3MzJhMDFmNjkwMjY4ZDNiOGViNzJlNWI4Y2NmOTRlMjIwMjE1MDA4NTkxMzExN2YyZTFhODUzMTUwNWVlOGNjZmM4ZTk4ZGYzY2YxNzQ4'
+}
